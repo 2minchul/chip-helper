ca2a77290e9c0a0babf3eeb55b55e9456c16924b
2minchul <2_minchul@naver.com>
2020-05-11 02:47:14 +0900

    delete unused files

--- qr_generator.py
+++
@@ -1,6 +0,0 @@
-import pyqrcode
-
-
-def generate(url, path):
-    img = pyqrcode.create(url, error='Q', version=3, mode='binary')
-    img.png(path, scale=7, quiet_zone=0)
--- run.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-C:\Users\drminchul\PycharmProjects\envs\naver-qr\Scripts\python.exe main.py
