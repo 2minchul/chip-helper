4c9b7975eb972385f60c4219f4f77e19a419f196
2minchul <2_minchul@naver.com>
2020-05-18 23:52:59 +0900

    카카오 미러서버 사용

--- setup.cmd
+++ setup.cmd
@@ -3,6 +3,6 @@ pushd %~dp0
 
 echo making venv ...
 py -3 -m venv venv
-venv\Scripts\pip install -U -r requirements.txt
+venv\Scripts\pip install -U -r requirements.txt -i http://ftp.daumkakao.com/pypi/simple --trusted-host ftp.daumkakao.com
 echo finish!
 pause
